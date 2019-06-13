f64e2fd83056e9ab9f1b12384a3dcc4ea5b965a2
s207230 <207230@edu.p.lodz.pl>
2019-06-13 23:10:56 +0200

    szud łork

--- Zadanie 5/src/main/java/Serialization/SerializationImpl.java
+++ Zadanie 5/src/main/java/Serialization/SerializationImpl.java
@@ -3,6 +3,8 @@ package Serialization;
 import Model.RecordCollection;
 import org.simpleframework.xml.Serializer;
 import org.simpleframework.xml.core.Persister;
+import org.simpleframework.xml.strategy.CycleStrategy;
+import org.simpleframework.xml.strategy.Strategy;
 import org.simpleframework.xml.stream.Format;
 
 import java.io.File;
@@ -11,7 +13,8 @@ public class SerializationImpl implements Serialization {
     @Override
     public void Serialize(RecordCollection obj, String path) {
         try {
-            Serializer serializer = new Persister(new Format("<?xml version='1.0' encoding='utf-8'?>"));
+            Strategy strategy = new CycleStrategy("zid","refzid");
+            Serializer serializer = new Persister(strategy, new Format("<?xml version='1.0' encoding='utf-8'?>"));
 
             File file = new File(path);
             serializer.write(obj, file);
@@ -24,7 +27,8 @@ public class SerializationImpl implements Serialization {
     @Override
     public RecordCollection Deserialize(String path) {
         try {
-            Serializer serializer = new Persister();
+            Strategy strategy = new CycleStrategy("zid","refzid");
+            Serializer serializer = new Persister(strategy);
             File file = new File(path);
             return serializer.read(RecordCollection.class, file);
         } catch (Exception e) {
